795595c99d881e775a99c537c89cdadb1afd0e4d
Tommy770221 <tommy770221@gmail.com>
2017-05-30 15:30:53 +0800

    1.新增 disease   CRUD 所有功能完善

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var methodOverride = require('method-override');
 var router = express.Router();
 var nodejieba = require("nodejieba");
 var paginate = require('express-paginate');
+var util = require('util');
+var validator = require('validator');
 
 
 var baseDir='/cbhJs';
@@ -37,6 +39,17 @@ app.use(baseDir,express.static(path.join(__dirname, 'public')));
 app.use(flash());
 app.use(session({secret:"secretpass123456"}));
 app.use(paginate.middleware(10, 50));
+app.use(expressValidator());
+
+
+app.use(methodOverride(function(req, res){
+    if (req.body && typeof req.body == 'object' && '_method' in req.body)
+    {
+        var method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}));
 
 app.use(baseDir+'/', index);
 app.use(baseDir+'/users', users);
@@ -45,7 +58,45 @@ app.use(baseDir+'/hospitalInfo', hospitalInfo);
 app.use(baseDir+'/line', lineBot);
 
 
+app.post('/:urlparam', function(req, res) {
 
+    // VALIDATION
+    // checkBody only checks req.body; none of the other req parameters
+    // Similarly checkParams only checks in req.params (URL params) and
+    // checkQuery only checks req.query (GET params).
+    req.checkBody('postparam', 'Invalid postparam').notEmpty().isInt();
+    req.checkParams('urlparam', 'Invalid urlparam').isAlpha();
+    req.checkQuery('getparam', 'Invalid getparam').isInt();
+
+    // OR assert can be used to check on all 3 types of params.
+    // req.assert('postparam', 'Invalid postparam').notEmpty().isInt();
+    // req.assert('urlparam', 'Invalid urlparam').isAlpha();
+    // req.assert('getparam', 'Invalid getparam').isInt();
+
+    // SANITIZATION
+    // as with validation these will only validate the corresponding
+    // request object
+    req.sanitizeBody('postparam').toBoolean();
+    req.sanitizeParams('urlparam').toBoolean();
+    req.sanitizeQuery('getparam').toBoolean();
+
+    // OR find the relevent param in all areas
+    req.sanitize('postparam').toBoolean();
+
+    // Alternatively use `var result = yield req.getValidationResult();`
+    // when using generators e.g. with co-express
+    req.getValidationResult().then(function(result) {
+        if (!result.isEmpty()) {
+            res.status(400).send('There have been validation errors: ' + util.inspect(result.array()));
+            return;
+        }
+        res.json({
+            urlparam: req.params.urlparam,
+            getparam: req.query.getparam,
+            postparam: req.body.postparam
+        });
+    });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -64,14 +115,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-app.use(methodOverride(function(req, res){
-    if (req.body && typeof req.body == 'object' && '_method' in req.body)
-    {
-        var method = req.body._method;
-        delete req.body._method;
-        return method;
-    }
-}));
 
 
 module.exports = app;
--- routes/disease.js
+++ routes/disease.js
@@ -2,65 +2,63 @@ var express = require('express');
 var router = express.Router();
 var paginate = require('express-paginate');
 var models = require('../models/index');
-
+var expressValidator = require('express-validator');
+var util = require('util');
 
 
 /* GET diseases listing. */
 router.get('/', function(req, res, next) {
 
+    models.Disease.count().then(function (count) {
+        models.Disease.findAll(
+            {
+                limit: req.query.limit,
+                offset: (req.query.page-1)*req.query.limit
+            }).then(function(diseases) {
+            var pageCount=Math.floor(count/req.query.limit)+1;
+            var pages = paginate.getArrayPages(req)(10, pageCount, req.query.page);
+            res.render('layout/disease/list',{title:"Customers",data:diseases, pages: pages,pageCount:pageCount});
+        }).catch(function (err) {
+            // handle error;
+            if(err){
+                var errornya  = ("Error Selecting : %s ",err );
+                console.log("errornya : "+errornya)
+                req.flash('msg_error', errornya);
+            }
+        });
+    })
 
-    models.Disease.findAll(
-       { limit: req.query.limit,
-         offset: (req.query.page-1)*req.query.limit
-       }).then(function(diseases) {
-        var pageCount=5;
-        var pages = paginate.getArrayPages(req)(3, pageCount, req.query.page);
-       res.render('layout/disease/list',{title:"Customers",data:diseases, pages: pages,pageCount:pageCount});
-      // res.json(todos[0]);
-   }).catch(function (err) {
-       // handle error;
-       if(err){
-           var errornya  = ("Error Selecting : %s ",err );
-           console.log("errornya : "+errornya)
-           req.flash('msg_error', errornya);
-       }
-   });;
 });
 
 router.post('/add', function(req, res, next) {
-    req.assert('name', 'Please fill the name').notEmpty();
+    req.assert('doc_category', 'Please fill the name').notEmpty();
     var errors = req.validationErrors();
     if (!errors) {
 
-
-        var disease = {
-            doc_category: req.body.doc_category,
-            category: req.body.category,
-            big_category: req.body.big_category,
-            symptom : req.body.symptom
-        }
-        models.Disease.save(disease);
-
-        var insert_sql = 'INSERT INTO customer SET ?';
-        req.getConnection(function(err,connection){
-            var query = connection.query(insert_sql, customer, function(err, result){
-                if(err)
-                {
-                    var errors_detail  = ("Error Insert : %s ",err );
-                    req.flash('msg_error', errors_detail);
-                    res.render('customer/add-customer',
-                        {
-                            name: req.param('name'),
-                            address: req.param('address'),
-                            email: req.param('email'),
-                            phone: req.param('phone'),
-                        });
-                }else{
-                    req.flash('msg_info', 'Create customer success');
-                    res.redirect('/customers');
-                }
+        models.sequelize.transaction(function (t,disease) {
+            disease = {
+                doc_category: req.body.doc_category,
+                category: req.body.category,
+                big_category: req.body.big_category,
+                symptom : req.body.symptom
+            }
+            // chain all your queries here. make sure you return them.
+            return models.Disease.create(disease, {transaction: t}).then(function (disease) {
+                console.log(disease);
+                return "result";
             });
+
+        }).then(function (result) {
+            // Transaction has been committed
+            // result is whatever the result of the promise chain returned to the transaction callback
+            console.log(result);
+            res.json({"success":"success"});
+        }).catch(function (err) {
+            // Transaction has been rolled back
+            // err is whatever rejected the promise chain returned to the transaction callback
         });
+
+
     }else{
         console.log(errors);
         errors_detail = "Sory there are error<ul>";
@@ -71,7 +69,7 @@ router.post('/add', function(req, res, next) {
         }
         errors_detail += "</ul>";
         req.flash('msg_error', errors_detail);
-        res.render('customer/add-customer',
+        res.render('layout/disease/list',
             {
                 name: req.param('name'),
                 address: req.param('address')
@@ -87,5 +85,56 @@ router.get('/add', function(req, res, next) {
         });
 });
 
+router.get('/edit/:id', function(req, res, next) {
+    var disease=models.Disease.findOne({
+        where: {
+            id: req.params.id,
+        }
+    }).then(function (disease) {
+        console.log(disease);
+        res.render( 'layout/disease/update',
+            {
+                title: 'Update  Disease',
+                disease:disease
+            });
+    });
+});
+
+router.post('/edit/:id', function(req, res, next) {
+    console.log(req.path.id);
+    var disease=models.Disease.findOne({
+        where: {
+            id: req.params.id,
+        },
+    }).then(function (disease) {
+        console.log(disease);
+
+        disease.update({
+            doc_category : req.body.doc_category,
+            big_category : req.body.big_category,
+            category : req.body.category,
+            symptom : req.body.symptom,
+        },null).then(function (disease) {
+            res.render( 'layout/disease/update',
+                {
+                    title: 'Update  Disease',
+                    disease:disease
+                });
+        });
+
+    });
+});
+
+router.delete('/delete/:id', function(req, res, next) {
+    var disease=models.Disease.findOne({
+        where: {
+            id: req.params.id,
+        }
+    }).then(function (disease) {
+        console.log(disease);
+        disease.destroy();
+        res.redirect(req.baseUrl);
+    });
+});
 
 module.exports = router;
